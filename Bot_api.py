ca4fd8206afea624336cb786efdc4c1376c6b613
“Valery” <117990439+ValeryManoilov@users.noreply.github.com>
2023-09-16 14:58:44 +0300

    Добавление всех файлов

---
+++ Bot_api.py
@@ -0,0 +1,17 @@
+from Config import Config
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import logging
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+bot = Bot(Config.BOT_TOKEN)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+dp.middleware.setup(LoggingMiddleware())
+
+@dp.message_handler()
+async def cmd_start(message: types.Message):
+    await message.answer()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ Config.py
@@ -0,0 +1,2 @@
+class Config:
+    BOT_TOKEN = '6510472205:AAFbgQLNdI_ZzUjdUkeXmLFUkYKn4l3ap60'
